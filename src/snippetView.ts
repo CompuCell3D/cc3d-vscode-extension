262fb462da0c156d1434916c8076dbe46d9c01f6
Pete Fyffe <pgf01@rocketmail.com>
2024-03-02 18:58:18 -0500

    Added setting showOnlyRelevantSnippetsInMenu to control TreeView

--- src/snippetView.ts
+++ src/snippetView.ts
@@ -1,115 +1,135 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import * as xmlTree from "./snippets/xml-tree.json";
 import * as cppTree from "./snippets/cpp-tree.json";
 import * as pythonTree from "./snippets/python-tree.json";
 
+function showOnlyRelevantSnippetsInMenu(): boolean {
+    return vscode.workspace.getConfiguration("cc3d").get("showOnlyRelevantSnippetsInMenu") as boolean;
+}
+
 export class SnippetView {
     treeDataProvider: TreeDataProvider;
 
+    handleLanguageChange(languageId: string) {
+        if (languageId === "python") {
+            this.treeDataProvider.enablePython();
+        } else if (languageId === "cpp") {
+            this.treeDataProvider.enableCpp();
+        } else if (languageId === "xml") {
+            this.treeDataProvider.enableXml();
+        }
+    }
+
     constructor(context: vscode.ExtensionContext) {
         this.treeDataProvider = new TreeDataProvider();
-        const view = vscode.window.createTreeView('snippetView', { treeDataProvider: this.treeDataProvider, showCollapseAll: true });
+        const view = vscode.window.createTreeView("snippetView", {
+            treeDataProvider: this.treeDataProvider,
+            showCollapseAll: true,
+        });
+
+        var langId = vscode.window.activeTextEditor?.document.languageId;
+        if (langId && showOnlyRelevantSnippetsInMenu()) {
+            this.handleLanguageChange(langId);
+        }
+
+        //Display the view
         context.subscriptions.push(view);
 
-        // Handle the file type change as the user navigates between files
+        //Handle the file type change as the user navigates between files
         vscode.window.onDidChangeActiveTextEditor((editor) => {
-          if (editor) {
-              const languageId = editor.document?.languageId;
+            if (editor && showOnlyRelevantSnippetsInMenu()) {
+                const languageId = editor.document?.languageId;
 
-              if (languageId === "python") {
-                this.treeDataProvider.enablePython();
-              }
-              else if (languageId === "cpp") {
-                this.treeDataProvider.enableCpp();
-              }
-              else if (languageId === "xml") {
-                this.treeDataProvider.enableXml();
-              }
-          }
-      });
+                this.handleLanguageChange(languageId);
+            }
+        });
     }
 }
 
-
 class TreeItem extends vscode.TreeItem {
-    children: TreeItem[]|undefined;
-  
-    constructor(label: string, children?: TreeItem[]) {
-      super(
-          label,
-          children == null ? vscode.TreeItemCollapsibleState.None :
-                                   vscode.TreeItemCollapsibleState.Expanded);
-      this.children = children;
-      //If this is a leaf node, then `snippetItem`
-      //indicates that there should be an icon/button for that item.
-      this.contextValue = children == null ? 'snippetItem' : 'treeMenu';
+    children: TreeItem[] | undefined;
 
-      this.command = {
-        command: "snippetView.selectNode",
-        title: "Select Node",
-        arguments: [label],
-      };
+    constructor(label: string, children?: TreeItem[]) {
+        super(
+            label,
+            children == null ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Expanded
+        );
+        this.children = children;
+        //If this is a leaf node, then `snippetItem`
+        //indicates that there should be an icon/button for that item.
+        this.contextValue = children == null ? "snippetItem" : "treeMenu";
+
+        this.command = {
+            command: "snippetView.selectNode",
+            title: "Select Node",
+            arguments: [label],
+        };
     }
 }
 
 function createTreeFromJson(jsonData: any, rootLabel: string) {
-	var mainChildren: TreeItem[] = [];
-	for (const [key, value] of Object.entries(jsonData)) {
-    var valueAny: any = value; //prevents TypeScript compiler error
-		var children: TreeItem[] = [];
-		for (var name of Object.keys(valueAny)) {
-			children.push(new TreeItem(name));
-		}
+    var mainChildren: TreeItem[] = [];
+    for (const [key, value] of Object.entries(jsonData)) {
+        var valueAny: any = value; //casting prevents TypeScript compiler error
+        var children: TreeItem[] = [];
+        for (var name of Object.keys(valueAny)) {
+            children.push(new TreeItem(name));
+        }
 
-		mainChildren.push(new TreeItem(key, children));
-	}; 
-	return new TreeItem(rootLabel, mainChildren);
+        mainChildren.push(new TreeItem(key, children));
+    }
+    return new TreeItem(rootLabel, mainChildren);
 }
 
 class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
     private changeEvent = new vscode.EventEmitter<void>();
     public get onDidChangeTreeData(): vscode.Event<void> {
-      return this.changeEvent.event;
+        return this.changeEvent.event;
     }
     public refresh(): void {
-      this.changeEvent.fire();
+        this.changeEvent.fire();
     }
-    
+
     public enableCpp() {
-      this.treeData = [createTreeFromJson(cppTree, 'CC3D C++')];
-      this.refresh();
+        this.treeData = [createTreeFromJson(cppTree, "CC3D C++")];
+        this.refresh();
     }
-    
+
     public enablePython() {
-      this.treeData = [createTreeFromJson(pythonTree, 'CC3D Python')];
-      this.refresh();
+        this.treeData = [createTreeFromJson(pythonTree, "CC3D Python")];
+        this.refresh();
     }
-    
+
     public enableXml() {
-      this.treeData = [createTreeFromJson(xmlTree, 'CC3DML')];
-      this.refresh();
+        this.treeData = [createTreeFromJson(xmlTree, "CC3DML")];
+        this.refresh();
     }
 
     treeData: TreeItem[];
-  
+
     constructor() {
-        this.treeData = [];
-        
-        // this.treeData = [new TreeItem('Code Snippets', [
-        //   createTreeFromJson(cppTree, 'CC3D C++'),
-        //   createTreeFromJson(xmlTree, 'CC3DML'),
-        //   createTreeFromJson(pythonTree, 'CC3D Python'),
-        // ])];
+        console.log('showOnlyRelevantSnippetsInMenu',showOnlyRelevantSnippetsInMenu())
+        if (showOnlyRelevantSnippetsInMenu()) {
+            this.treeData = [];
+        } else {
+            this.treeData = [
+                new TreeItem("Code Snippets", [
+                    createTreeFromJson(cppTree, "CC3D C++"),
+                    createTreeFromJson(xmlTree, "CC3DML"),
+                    createTreeFromJson(pythonTree, "CC3D Python"),
+                ]),
+            ];
+        }
     }
-  
+
     getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
-      return element;
+        return element;
     }
-  
-    getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
-      if (element === undefined) {
-        return this.treeData;
-      }
-      return element.children;
+
+    getChildren(element?: TreeItem | undefined): vscode.ProviderResult<TreeItem[]> {
+        if (element === undefined) {
+            return this.treeData;
+        }
+        return element.children;
     }
 }
